51608357cc8d4955e24c87033d4aea2aca06d39f
Yuuki Galaxy <galaxy001@gmail.com>
2012-08-31 16:46:32 +0800

    add www.cloudcore.jp/vps/kumonocoa2012/

---
+++ Pictures/kumonocoa2012.Hikaru/js/scripts.js
@@ -0,0 +1,5 @@
+function fbs_Twitter() {
+    var title = 'CloudCore VPS四月馬鹿  雲野コア誕生　完全模倣';
+    var url = 'http://www.cloudcore.jp/vps/kumonocoa2012/';
+    void (window.open('http://twitter.com/home/?status='.concat(encodeURIComponent(title)).concat(' ').concat(encodeURIComponent(url))));
+}
