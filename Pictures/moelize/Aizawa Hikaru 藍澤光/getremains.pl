ac1f369e2bd2a1ae9f64d1f95410a51aa516ca6d
Yuuki Galaxy <galaxy001@gmail.com>
2014-10-06 02:01:48 +0800

    new file:   20140427/taiwan/silverlight/desktop_l_1024x768.htm 	modified:   getremains.pl

--- Pictures/moelize/Aizawa Hikaru 藍澤光/getremains.pl
+++ Pictures/moelize/Aizawa Hikaru 藍澤光/getremains.pl
@@ -1,11 +1,29 @@
 #!/bin/perl
 
-my @sizes = qw[ 1024x768 1280x1024 1280x800 1440x900 1366x768 1600x900 1920x1080 ];
-my $prefix = 'http://www.microsoft.com/taiwan/silverlight/desktop_';
+my ($type,$in) = @ARGV;
 
-for my $in (@ARGV) {
-	for my $size (@sizes) {
-		my $t = "proxychains -q wget -mkEpnp $prefix${in}_$size.htm";
-		system "$t\n";
-	}
+my (@sizes,$prefix,$suffix);
+if ($type eq 'h') {
+	@sizes = qw[ 1024x768 1280x1024 1280x800 1440x900 1366x768 1600x900 1920x1080 ];
+	$prefix = 'http://www.microsoft.com/taiwan/silverlight/desktop_';
+	$suffix = '.htm';
+} elsif ($type eq 'p') {
+	@sizes = qw[ 1024X768 1280x1024 1280x800 1440x900 1366X768 1600x900 1920X1080 ];
+	$prefix = 'http://www.microsoft.com/taiwan/silverlight/images/';
+	$suffix = '.jpg';
 }
+
+for my $size (@sizes) {
+	my $t;
+	if ($type eq 'h') {
+		$t = "proxychains -q wget -mkEpnp $prefix${in}_${size}$suffix";
+	} elsif ($type eq 'p') {
+		$t = "proxychains -q wget -mkEpnp $prefix${size}_${in}$suffix";
+	}
+	print "$t\n";
+	system "$t";
+}
+
+__END__
+h j,l,m
+p i
